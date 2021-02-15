8c6b39ac497585e6fe5e8f1d6ece892018dac45a
EtcGonza <xetchegaraygonzalo@gmail.com>
2021-02-15 18:12:48 -0300

    Creo tarea service.

---
+++ src/app/services/tarea.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Tarea } from '../models/Tarea';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TareaService {
+
+  constructor(private httpClient: HttpClient) {}
+
+  async crearTarea(tarea: Tarea): Promise<Observable<string>> {
+    if (tarea.idtarea) {
+      return this.httpClient.put<string>('Tarea', tarea);
+    } else {
+      return this.httpClient.post<string>('Tarea', tarea);
+    }
+  }
+
+  async getTareasProyecto(idProyecto: number): Promise<Observable<Tarea[]>> {
+      return this.httpClient.get<Tarea[]>(`Tarea/${idProyecto}`);
+  }
+
+  async borrarTarea(idTarea: number): Promise<Observable<string>> {
+      return this.httpClient.delete<string>(`Tarea/${idTarea}`);
+  }
+}
